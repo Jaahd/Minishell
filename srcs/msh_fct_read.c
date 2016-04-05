8df80a830e9e1dde04ba1288810c91b2bf209e9d
Anne-Laure VACHER <avacher@student.42.fr>
2016-04-05 19:20:43 +0200

    norme ok \?

--- srcs/msh_fct_read.c
+++ srcs/msh_fct_read.c
@@ -15,29 +15,47 @@
 #include "minishell.h"
 #include "libft.h"
 
-char		**read_n_check(char *special, char *read_buff)
+//		if (read_buff[i] == special[j] && tmp[0] != '\0')
+//		{
+//			ft_lstpushback(&arg, tmp);
+//			ft_bzero(tmp, 1024);
+//			k = 0;
+//		}
+//		else if (read_buff[i] != special[j])
+//			tmp[k++] = read_buff[i];
+
+static int		create_list(char cheat[], char (*tmp)[], int *k, t_list **arg)
+{
+	if (cheat[0] == cheat[1] && (*tmp)[0] != '\0')
+	{
+		ft_lstpushback(arg, *tmp);
+		ft_bzero(*tmp, 1024);
+		*k = 0;
+	}
+	else if (cheat[0] != cheat[1])
+		(*tmp)[(*k)++] = cheat[0];
+	return (0);
+}
+char			**read_n_check(char *special, char *read_buff)
 {
 	t_list			*arg;
 	char			tmp[1024];
+	char			cheat[2];
 	char			**tbl;
-	static int		i[] = {-1, 0, 0};
+	int				i[3];
 
 	ft_bzero(tmp, 1024);
 	arg = NULL;
 	i[0] = -1;
+	i[2] = 0;
 	while (read_buff[++i[0]])
 	{
 		i[1] = 0;
 		while (read_buff[i[0]] != special[i[1]] && special[i[1]])
 			i[1]++;
-		if (read_buff[i[0]] == special[i[1]] && tmp[0] != '\0')
-		{
-			ft_lstpushback(&arg, tmp);
-			ft_bzero(tmp, 1024);
-			i[2] = 0;
-		}
-		else if (read_buff[i[0]] != special[i[1]])
-			tmp[i[2]++] = read_buff[i[0]];
+		cheat[0] = read_buff[i[0]];
+		cheat[1] = special[i[1]];
+		create_list(cheat, &tmp, &(i[2]), &arg);
 	}
 	if (ft_strlen(tmp))
 		ft_lstpushback(&arg, tmp);
@@ -46,7 +64,7 @@ char		**read_n_check(char *special, char *read_buff)
 	return (tbl);
 }
 
-int			check_home(char **cmd)
+int				check_home(char **cmd)
 {
 	int			i;
 
@@ -65,7 +83,7 @@ int			check_home(char **cmd)
 	return (0);
 }
 
-int			fct_read(char *read_buff, char **env, t_duo **env_cpy)
+int				fct_read(char *read_buff, char **env, t_duo **env_cpy)
 {
 	int				ret;
 	char			**cmd;
